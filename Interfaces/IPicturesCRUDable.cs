0accc818bf2b23d32a60c3161ab1e541b27335a9
bermet <116433177+tartapie@users.noreply.github.com>
2022-11-26 15:48:08 +0600

    создали ICRUD интерфейс
    я красотка

---
+++ Interfaces/IPicturesCRUDable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Interfaces
+{
+    public interface IPicturesCRUDable
+    {
+        public Task<IActionResult> Index();
+        [HttpPost]
+        //добавление элементов для главнйо страницы , включая медиафайлы
+        public Task<IActionResult> Create(string NewsName, string newsDescription, IFormFileCollection uploadedFiles);
+        public Task<IActionResult> Create();
+        [HttpPost]
+        //Удаление
+        public Task<IActionResult> Delete(int id);
+        public Task<IActionResult> SetMainPictire(int IsMain);
+    }
+}
---
+++ Migrations/20221126092703_AddNewFieldIsMain.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdminPanel.Migrations
+{
+    public partial class AddNewFieldIsMain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMain",
+                table: "Pictures",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMain",
+                table: "Pictures");
+        }
+    }
+}
